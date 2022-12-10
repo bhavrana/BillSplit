84ddc6cb8365374bd99b9ff24987d112b5d05602
bhavrana <bhavna.rana21@gmail.com>
2022-12-10 14:25:43 +0900

    Initial commit

---
+++ src/test/java/com/example/billsplit/BillSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billsplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillSplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
